2f062da0ae4dda125283ff067613010732d0310c
divyansh1211 <divyanshmanchanda1211@gmail.com>
2024-01-25 04:43:56 +0530

    counter added

---
+++ 02count/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15);
+  
+  // let counter = 15;
+
+  const increment = () =>{
+    setCounter(counter + 1)
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+    <h1>React Counter {counter}</h1>
+    <h2>Counter Value: {counter}</h2> 
+    <button
+    onClick={increment}>Add Value</button>{" "}
+    <button
+    onClick={decrement}>Remove Value</button>
+    <footer>Footer: {counter}</footer>       
+    </>
+  )
+}
+
+export default App
